1428f1c961c482d6d6f70a26339f5373d6db7a5e
bluceWang <747092564@qq.com>
2017-04-23 10:03:17 +0800

    [chg]test

--- src/com/blucewang/chapter10_innerClass/AnonymousConstructor.java
+++ src/com/blucewang/chapter10_innerClass/AnonymousConstructor.java
@@ -4,25 +4,30 @@ package com.blucewang.chapter10_innerClass;//: innerclasses/AnonymousConstructor
 import static com.blucewang.net.mindview.util.Print.*;
 
 abstract class Base {
-  public Base(int i) {
-    print("Base constructor, i = " + i);
-  }
-  public abstract void f();
+    public Base(int i) {
+        print("Base constructor, i = " + i);
+    }
+
+    public abstract void f();
 }
 
 public class AnonymousConstructor {
-  public static Base getBase(int i) {
-    return new Base(i) {
-      { print("Inside instance initializer"); }
-      public void f() {
-        print("In anonymous f()");
-      }
-    };
-  }
-  public static void main(String[] args) {
-    Base base = getBase(47);
-    base.f();
-  }
+    public static Base getBase(int i) {
+        return new Base(i) {
+            {
+                print("Inside instance initializer");
+            }
+
+            public void f() {
+                print("In anonymous f()");
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        Base base = getBase(47);
+        base.f();
+    }
 } /* Output:
 Base constructor, i = 47
 Inside instance initializer
