a92d25b312c6f8c2f93be13a1907960626c9ea4c
wangyb <wangyb@hlsteel.com>
2017-12-29 17:26:19 +0800

    [chg]增加多线程交替打印代码

---
+++ src/com/blucewang/multiThread/extra/concurrentUtil/SimpleSynchronizer.java
@@ -0,0 +1,9 @@
+package com.blucewang.multiThread.extra.concurrentUtil;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+/**
+ * Created by bluceWang on 2017/12/28.
+ */
+public class SimpleSynchronizer extends AbstractQueuedSynchronizer{
+}
